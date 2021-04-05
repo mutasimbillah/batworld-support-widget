51ff9bb83ed45da9ab53af72e6eded50825534ba
Mutasim Billah <billahbubt@gmail.com>
2021-04-05 15:47:03 +0600

    api JS

---
+++ api.js
@@ -0,0 +1,32 @@
+function requestServerForJS() {
+    var s1 = document.createElement("script");
+    var url =
+      "https://testapi-k8s.oss.net.bd/api/feedback-service/contact-widget/get-js?clientId=b54aa8cb-b96e-43e6-bbd0-59f26cdf4f67";
+    s1.setAttribute("src", url);
+    document.body.appendChild(s1);
+  }
+
+function batworldCheckApi() {
+    var checkUrl =
+      "https://testapi-k8s.oss.net.bd/api/feedback-service/contact-widget/reload-js";
+    var apiOptions = {
+      method: "GET",
+      headers: {
+        "client-id": "b54aa8cb-b96e-43e6-bbd0-59f26cdf4f67",
+      },
+      referer: "https://clo-stage.land.gov.bd",
+    };
+    fetch(checkUrl, apiOptions)
+      .then((response) => response)
+      .then((data) => {
+        data = {
+          relaod: false,
+        };
+        console.log(data.relaod);
+        if (data.relaod === true) {
+          requestServerForJS();
+        }
+      });
+  }
+ 
+setInterval(batworldCheckApi, 15000);
